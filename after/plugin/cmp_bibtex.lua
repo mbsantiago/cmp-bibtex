4bcf49a60f5a48e41eba66c8067adcdc9c2acf52
mbsantiago <santiago.mbal@gmail.com>
2024-07-08 01:11:26 +0100

    Load on enter typst buffer

--- after/plugin/cmp_bibtex.lua
+++ after/plugin/cmp_bibtex.lua
@@ -1,1 +1,7 @@
-require('cmp').register_source('bibtex', require('cmp_bibtex'))
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("cmp_bibtex", { clear = true }),
+  pattern = "*.typ",
+  callback = function()
+    require("cmp").register_source("bibtex", require("cmp_bibtex"))
+  end,
+})
